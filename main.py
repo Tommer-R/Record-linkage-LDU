905821acaf427257cbbce0358f54dbb55f7fe752
Tommer-R <76598250+Tommer-R@users.noreply.github.com>
2021-12-07 05:46:40 +0100

    functional

--- main.py
+++ main.py
@@ -1,152 +1,91 @@
+from pre_processing import hw, lda, hw_raw, lda_raw
 import pandas as pd
 import numpy as np
 import recordlinkage as rl
 import textdistance as td
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-
-nltk.download('stopwords')
-nltk.download('punkt')
-
-all_stopwords = stopwords.words('english')
-all_stopwords.append('&')
-address_stopwords = all_stopwords + ["street", "st", "place", "rd", "road", 'square']
-name_stopwords = all_stopwords + ['co', 'corp', 'inc', 'company', 'limited', 'llc']
-
-lda = pd.read_csv('Priority Customers.csv', delimiter=';')
-hw = pd.read_csv('HeroWeb Accounts.csv', delimiter=';')
-
-lda.columns = [c.lower() for c in list(lda.columns)]  # change to lower case
-hw.columns = [c.lower() for c in list(hw.columns)]  # change to lower case
-hw.columns = [c.replace('account_', '') for c in list(hw.columns)]  # simplify column names
-hw_columns_to_drop = ['active', 'date_joined', 'date_expires', 'referred_by', 'locked', 'terms', 'sales_rep',
-                      'is_sales_rep', 'tax_id', 'tax_exempt', 'long', 'lat', 'date_last_ordered', 'total_orders',
-                      'total_revenue', 'notes', 'store_optin']
-lda_columns_to_drop = ['city & state']
-
-hw.drop(columns=hw_columns_to_drop, inplace=True)
-lda.drop(columns=lda_columns_to_drop, inplace=True)
-
-# rename columns to match
-"""
-HeroWeb
-
-email_address > email
-telephone > phone
-stelephone >
-saddress1 > 
-saddress2 > 
-scity > 
-sstate > 
-szip > 
-scountry > 
-telephone2 > phone2
-"""
-hw.columns = ['id', 'email', 'company_name', 'last_name', 'first_name', 'name', 'group', 'phone',
-              'address1', 'address2', 'city', 'state', 'zip', 'country', 'phone2', 'saddress1',
-              'saddress2', 'scity', 'sstate', 'szip', 'scountry', 'sphone2']
-
-"""
-LandsDownUnder
-
-number > id
-phone no. > phone
-fax no. > phone
-e-mail > email
-group description > group
-street address > address1
-address (line 2) > address2
-address (line 3) > address3
-zip code > zip
-hw account > hw id
-"""
-lda.columns = ['id', 'name', 'phone', 'fax', 'email', 'group', 'address1', 'address2', 'address3', 'city',
-               'state code', 'state', 'zip', 'country', 'web_site', 'hw id']
-
-lda_des = lda.describe()
-hw_des = hw.describe()
+from progressbar import progressbar
 
 lda.info()
 hw.info()
 
+indexer = rl.Index()
+indexer.full()
+possible_links = indexer.index(hw, lda)
 
-def normalize_name(a: str) -> list[str]:
-    a = a.lower()  # convert to lower case
-    a = re.sub(r'[^a-z0-9 ]', '', a)  # keep numbers and letters and space
-    tokens = word_tokenize(a)  # separate words
-    tokens = [word for word in tokens if word not in all_stopwords]  # remove stopwords
-    return tokens
+hw_to_lda = {
+    'email': ['email'],
+    'company_name': ['name'],
+    'name1': ['name'],
+    'name2': ['name'],
+    'group': ['group'],
+    'phone': ['phone', 'fax'],
+    'address1': ['address'],
+    'address2': ['address'],
+    'city': ['city'],
+    'state': ['state code', 'state'],
+    'zip': ['zip'],
+    'country': ['country'],
+    'phone2': ['phone', 'fax'],
+    'address3': ['address'],
+    'address4': ['address'],
+    'city2': ['city'],
+    'state2': ['state'],
+    'zip2': ['zip'],
+    'country2': ['country'],
+    'phone3': ['phone', 'fax']
+}
+
+property_merge = {
+    'name': ['name1', 'name2'],
+    'address': ['address1', 'address2', 'address3', 'address4'],
+    'city': ['city', 'city2'],
+    'state': ['state', 'state2'],
+    'zip': ['zip', 'zip2'],
+    'country': ['country', 'country2'],
+    'phone': ['phone', 'phone2', 'phone3']
+}
+
+compare_method = {
+    'tokens': td.jaccard,
+    'string': td.levenshtein.normalized_similarity
+}
 
 
-def normalize_address(a: str) -> list[str]:
-    a = a.lower()  # convert to lower case
-    a = re.sub(r'[^a-z0-9 ]', '', a)  # keep numbers and letters and space
-    tokens = word_tokenize(a)  # separate words
-    tokens = [word for word in tokens if word not in address_stopwords]  # remove stopwords
-    return tokens
+def compare_records(rec1, rec2):
+    scores = {}
+
+    for col in rec1.index:
+        if col not in set(hw_to_lda.keys()):
+            continue
+        temp_scores = []
+        for col2 in hw_to_lda[col]:
+            if type(rec1[col]) == float and not pd.notnull(rec1[col]) or \
+                    type(rec2[col2]) == float and not pd.notnull(rec2[col2]):
+                temp_scores.append(0)
+            elif type(rec1[col]) == list and type(rec2[col2]) == list:
+                temp_scores.append(compare_method['tokens'](rec1[col], rec2[col2]))
+
+            elif type(rec1[col]) == str and type(rec2[col2]) == str:
+                temp_scores.append(compare_method['string'](rec1[col], rec2[col2]))
+
+        scores[col] = float(max(temp_scores))
+
+    for k, v in property_merge.items():
+        scores[k] = float(max([scores[a] for a in v]))
+        for a in v:
+            if a != k:
+                scores.pop(a)
+
+    return scores
 
 
-def normalize_number(a) -> str:
-    a = re.sub(r'[^0-9]', '', a)  # keep only numbers
-    return a
+scores = pd.DataFrame(index=possible_links, columns=list(compare_records(hw.loc[1], lda.loc[1]).keys()))
+for pair in progressbar(possible_links):
+    scores.loc[pair] = compare_records(hw.loc[pair[0]], lda.loc[pair[1]])
 
+scores['total'] = scores.sum(axis=1)
 
-def normalize_email(a) -> list[str]:
-    a = a.lower()  # convert to lower case
-    a = re.sub(r'[^a-z0-9 ]', ' ', a)  # keep numbers and letters and space
-    tokens = word_tokenize(a)  # separate words
-    tokens = [word for word in tokens if word not in all_stopwords]  # remove stopwords
-    return tokens
+scores = scores.astype(float)
+description = scores.describe()
 
-
-def remove_www(a):
-    if type(a) == list and 'www' in a:
-        a.remove('www')
-    return a
-
-
-lda_raw = lda.copy()
-hw_raw = hw.copy()
-
-# normalize lda
-lda['name'] = lda['name'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
-lda['phone'] = lda['phone'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
-lda['fax'] = lda['fax'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
-lda['email'] = lda['email'].apply(lambda x: normalize_email(x) if pd.notnull(x) else x)
-lda['group'] = lda['group'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
-lda['address1'] = lda['address1'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-lda['address2'] = lda['address2'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-lda['address3'] = lda['address3'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-lda['city'] = lda['city'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-lda['state code'] = lda['state code'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-lda['state'] = lda['state'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-lda['zip'] = lda['zip'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
-lda['country'] = lda['country'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-lda['web_site'] = lda['web_site'].apply(lambda x: normalize_email(x) if pd.notnull(x) else x)
-lda['web_site'] = lda['web_site'].apply(lambda x: remove_www(x) if x != np.nan else x)  # remove 'www'
-
-# normalize hw
-hw['email'] = hw['email'].apply(lambda x: normalize_email(x) if pd.notnull(x) else x)
-hw['company_name'] = hw['company_name'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
-hw['last_name'] = hw['last_name'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
-hw['first_name'] = hw['first_name'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
-hw['name'] = hw['name'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
-hw['group'] = hw['group'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
-hw['phone'] = hw['phone'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
-hw['address1'] = hw['address1'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-hw['address2'] = hw['address2'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-hw['city'] = hw['city'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-hw['state'] = hw['state'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-hw['zip'] = hw['zip'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
-hw['country'] = hw['country'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-hw['phone2'] = hw['phone2'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
-hw['saddress1'] = hw['saddress1'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-hw['saddress2'] = hw['saddress2'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-hw['scity'] = hw['scity'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-hw['sstate'] = hw['sstate'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-hw['szip'] = hw['szip'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
-hw['scountry'] = hw['scountry'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
-hw['sphone2'] = hw['sphone2'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
 
---
+++ pre_processing.py
@@ -0,0 +1,241 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from itertools import product
+
+# nltk.download('stopwords')
+# nltk.download('punkt')
+
+all_stopwords = stopwords.words('english')
+all_stopwords.append('&')
+state_codes = ['ak', 'al', 'ar', 'az', 'ca', 'co', 'ct', 'dc', 'de', 'fl', 'ga', 'hi', 'ia', 'id', 'il', 'in', 'ks',
+               'ky', 'la', 'ma', 'md', 'me', 'mi', 'mn', 'mo', 'ms', 'mt', 'nc', 'nd', 'ne', 'nh', 'nj', 'nm', 'nv',
+               'ny', 'oh', 'ok', 'or', 'pa', 'ri', 'sc', 'sd', 'tn', 'tx', 'ut', 'va', 'vt', 'wa', 'wi', 'wv', 'wy']
+
+for s in state_codes:
+    try:
+        all_stopwords.remove(s)
+    except ValueError:
+        pass
+
+address_stopwords = all_stopwords + ["street", "st", "place", "rd", "road", 'square']
+name_stopwords = all_stopwords + ['co', 'corp', 'inc', 'company', 'limited', 'llc']
+
+lda = pd.read_csv('Priority Customers.csv', delimiter=';')
+hw = pd.read_csv('HeroWeb Accounts.csv', delimiter=';')
+
+lda = lda[:100]
+hw = hw[:100]
+
+lda.columns = [c.lower() for c in list(lda.columns)]  # change to lower case
+hw.columns = [c.lower() for c in list(hw.columns)]  # change to lower case
+hw.columns = [c.replace('account_', '') for c in list(hw.columns)]  # simplify column names
+hw_columns_to_drop = ['active', 'date_joined', 'date_expires', 'referred_by', 'locked', 'terms', 'sales_rep',
+                      'is_sales_rep', 'tax_id', 'tax_exempt', 'long', 'lat', 'date_last_ordered', 'total_orders',
+                      'total_revenue', 'notes', 'store_optin']
+lda_columns_to_drop = ['city & state']
+
+hw.drop(columns=hw_columns_to_drop, inplace=True)
+lda.drop(columns=lda_columns_to_drop, inplace=True)
+
+# rename columns to match
+"""
+HeroWeb
+
+email_address > email
+name > name2
+telephone > phone
+stelephone >
+saddress1 > 
+saddress2 > 
+scity > 
+sstate > 
+szip > 
+scountry > 
+telephone2 > phone2
+"""
+hw.columns = ['id', 'email', 'company_name', 'last_name', 'first_name', 'name2', 'group', 'phone',
+              'address1', 'address2', 'city', 'state', 'zip', 'country', 'phone2', 'saddress1',
+              'saddress2', 'city2', 'state2', 'zip2', 'country2', 'phone3']
+
+"""
+LandsDownUnder
+
+number > id
+phone no. > phone
+fax no. > phone
+e-mail > email
+group description > group
+street address > address1
+address (line 2) > address2
+address (line 3) > address3
+zip code > zip
+hw account > hw id
+"""
+lda.columns = ['id', 'name', 'phone', 'fax', 'email', 'group', 'address1', 'address2', 'address3', 'city',
+               'state code', 'state', 'zip', 'country', 'web_site', 'hw id']
+
+lda_des = lda.describe()
+hw_des = hw.describe()
+
+
+def normalize_name(a: str) -> list[str]:
+    a = a.lower()  # convert to lower case
+    a = re.sub(r'[^a-z0-9 ]', '', a)  # keep numbers and letters and space
+    tokens = word_tokenize(a)  # separate words
+    tokens = [word for word in tokens if word not in all_stopwords]  # remove stopwords
+    return tokens
+
+
+def normalize_address(a: str) -> list[str]:
+    a = a.lower()  # convert to lower case
+    a = re.sub(r'[^a-z0-9 ]', '', a)  # keep numbers and letters and space
+    tokens = word_tokenize(a)  # separate words
+    tokens = [word for word in tokens if word not in address_stopwords]  # remove stopwords
+    return tokens
+
+
+def normalize_number(a) -> str:
+    a = re.sub(r'[^0-9]', '', a)  # keep only numbers
+    return a
+
+
+def normalize_email(a) -> list[str]:
+    a = a.lower()  # convert to lower case
+    a = re.sub(r'[^a-z0-9 ]', ' ', a)  # keep numbers and letters and space
+    tokens = word_tokenize(a)  # separate words
+    tokens = [word for word in tokens if word not in all_stopwords]  # remove stopwords
+    return tokens
+
+
+def remove_www(a):
+    if type(a) == list and 'www' in a:
+        a.remove('www')
+    return a
+
+
+def merge_columns(df, col1, col2, drop=True):
+    temp_df = pd.DataFrame(index=list(df.index), columns=[col1])
+    for i in df.index:
+        if type(df.loc[i, col1]) == list and type(df.loc[i, col2]) == list:
+            df.loc[i, col1].extend(df.loc[i, col2])
+            temp_df.loc[i, col1] = df.loc[i, col1]
+        elif type(df.loc[i, col1]) == str and type(df.loc[i, col2]) == str:
+            temp_df.loc[i, col1] = [df.loc[i, col1], df.loc[i, col2]]
+        elif type(df.loc[i, col1]) == str and type(df.loc[i, col2]) == list:
+            df.loc[i, col2].append(df.loc[i, col1])
+            temp_df.loc[i, col1] = df.loc[i, col2]
+        elif type(df.loc[i, col1]) == list and type(df.loc[i, col2]) == str:
+            df.loc[i, col1].append(df.loc[i, col2])
+            temp_df.loc[i, col1] = df.loc[i, col1]
+        elif type(df.loc[i, col1]) == float and not pd.notnull(df.loc[i, col1]) and type(df.loc[i, col2]) == list:
+            temp_df.loc[i, col1] = df.loc[i, col2]
+        else:
+            temp_df.loc[i, col1] = df.loc[i, col1]
+
+    df[col1] = temp_df[col1]
+
+    if drop:
+        df.drop(columns=[col2], inplace=True)
+    return df
+
+
+lda_raw = lda.copy()
+hw_raw = hw.copy()
+
+# normalize lda
+lda['name'] = lda['name'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
+lda['phone'] = lda['phone'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
+lda['fax'] = lda['fax'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
+lda['email'] = lda['email'].apply(lambda x: normalize_email(x) if pd.notnull(x) else x)
+lda['group'] = lda['group'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
+lda['address1'] = lda['address1'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+lda['address2'] = lda['address2'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+lda['address3'] = lda['address3'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+lda['city'] = lda['city'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+lda['state code'] = lda['state code'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+lda['state'] = lda['state'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+lda['zip'] = lda['zip'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
+lda['country'] = lda['country'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+lda['web_site'] = lda['web_site'].apply(lambda x: normalize_email(x) if pd.notnull(x) else x)
+lda['web_site'] = lda['web_site'].apply(lambda x: remove_www(x) if x != np.nan else x)  # remove 'www'
+
+# normalize hw
+hw['email'] = hw['email'].apply(lambda x: normalize_email(x) if pd.notnull(x) else x)
+hw['company_name'] = hw['company_name'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
+hw['last_name'] = hw['last_name'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
+hw['first_name'] = hw['first_name'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
+hw['name2'] = hw['name2'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
+hw['group'] = hw['group'].apply(lambda x: normalize_name(x) if pd.notnull(x) else x)
+hw['phone'] = hw['phone'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
+hw['address1'] = hw['address1'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+hw['address2'] = hw['address2'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+hw['city'] = hw['city'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+hw['state'] = hw['state'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+hw['zip'] = hw['zip'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
+hw['country'] = hw['country'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+hw['phone2'] = hw['phone2'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
+hw['saddress1'] = hw['saddress1'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+hw['saddress2'] = hw['saddress2'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+hw['city2'] = hw['city2'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+hw['state2'] = hw['state2'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+hw['zip2'] = hw['zip2'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
+hw['country2'] = hw['country2'].apply(lambda x: normalize_address(x) if pd.notnull(x) else x)
+hw['phone3'] = hw['phone3'].apply(lambda x: normalize_number(x) if pd.notnull(x) else x)
+
+# convert to string
+for col, i in product(list(hw.columns), hw.index):
+    if type(hw[col][i]) == float and pd.notnull(hw[col][i]):
+        hw.loc[i, col] = str(hw.loc[i, col])
+
+# hw = merge_columns(hw, 'address1', 'address2')
+# hw = merge_columns(hw, 'saddress1', 'saddress2')
+hw = merge_columns(hw, 'first_name', 'last_name')
+
+hw.columns = ['name1' if x == 'first_name' else x for x in hw.columns]
+hw.columns = ['address3' if x == 'saddress1' else x for x in hw.columns]
+hw.columns = ['address4' if x == 'saddress2' else x for x in hw.columns]
+
+# remove duplicate values within a record
+for i in hw.index:
+    if hw.loc[i, 'address1'] == hw.loc[i, 'address2']:
+        hw.loc[i, 'address2'] = np.nan
+
+    if hw.loc[i, 'city'] == hw.loc[i, 'city2'] and type(hw.loc[i, 'address2']) == float and \
+            not pd.notnull(hw.loc[i, 'address2']):
+        hw.loc[i, 'city2'] = np.nan
+
+    if hw.loc[i, 'state'] == hw.loc[i, 'state2'] and type(hw.loc[i, 'address2']) == float and \
+            not pd.notnull(hw.loc[i, 'address2']):
+        hw.loc[i, 'state2'] = np.nan
+
+    if hw.loc[i, 'zip'] == hw.loc[i, 'zip2'] and type(hw.loc[i, 'address2']) == float and \
+            not pd.notnull(hw.loc[i, 'address2']):
+        hw.loc[i, 'zip2'] = np.nan
+
+    if hw.loc[i, 'country'] == hw.loc[i, 'country2'] and type(hw.loc[i, 'address2']) == float and \
+            not pd.notnull(hw.loc[i, 'address2']):
+        hw.loc[i, 'country2'] = np.nan
+
+    if hw.loc[i, 'phone2'] == hw.loc[i, 'phone3'] or hw.loc[i, 'phone'] == hw.loc[i, 'phone3'] and \
+            type(hw.loc[i, 'address2']) == float and not pd.notnull(hw.loc[i, 'address2']):
+        hw.loc[i, 'phone3'] = np.nan
+
+    if hw.loc[i, 'phone'] == hw.loc[i, 'phone2'] and type(hw.loc[i, 'address2']) == float and \
+            not pd.notnull(hw.loc[i, 'address2']):
+        hw.loc[i, 'phone2'] = np.nan
+
+lda = merge_columns(lda, 'address1', 'address2')
+lda = merge_columns(lda, 'address1', 'address3')
+
+lda.columns = ['address' if x == 'address1' else x for x in lda.columns]
+
+hw.replace([], np.nan, inplace=True)
+lda.replace([], np.nan, inplace=True)
+
+#hw.reindex(columns=['id', 'email', 'company_name', 'name1', 'name2', 'group', 'phone', 'phone2', 'phone3',
+#                    'address1', 'city', 'state', 'zip', 'country', 'address2',
+#                    'city2', 'state2', 'zip2', 'country2'])
